05bb6b1b449b7cee1a3eabd54fb9e1bbafa2d1ac
James Woolfenden <james.woolfenden@gmail.com>
2023-02-06 13:32:11 +0000

    switch roles

--- example/state-writer/module.role.tf
+++ example/state-writer/module.role.tf
@@ -1,7 +1,7 @@
 module "example" {
   source = "../../"
-  assume = data.aws_iam_policy_document.statewriter.json
-  policy = data.aws_iam_policy_document.assume.json
+  assume = data.aws_iam_policy_document.assume.json
+  policy = data.aws_iam_policy_document.statewriter.json
   role   = "state-writer"
   tags = {
     pike = "permissions"
